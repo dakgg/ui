5608c49ca903b4dc19c5696a667ceb587b9a3e7a
dakgg <hhs1992@naver.com>
2026-06-19 22:21:17 +0900

    clean code

--- Runtime/Core/ViewBase.cs
+++ Runtime/Core/ViewBase.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace ViewSystem
--- Runtime/Core/ViewCallbackInterfaces.cs
+++ Runtime/Core/ViewCallbackInterfaces.cs
@@ -1,6 +1,4 @@
-
 using System.Collections;
-using UnityEngine;
 
 namespace ViewSystem
 {
